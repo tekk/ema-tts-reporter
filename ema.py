a9e0566733a0b164bafb6f8e7b9e7abb1417039a
tekk <tekk.sk@gmail.com>
2020-05-24 16:41:41 +0200

    First RPi version

--- ema.py
+++ ema.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import requests, gtts, bs4, configparser, playsound
+import requests, gtts, gpiozero, configparser, playsound
 from metar import Metar
 
 BASE_URL = "http://tgftp.nws.noaa.gov/data/observations/metar/stations"
@@ -9,21 +9,21 @@ def main():
     config.read('ema.ini')
     airport = config['DEFAULT']['Airport']
     language = config['DEFAULT']['Language']
+    port = config['DEFAULT']['PTT-Pin']
+    ptt = gpiozero.LED(port)
+    ptt.off()
     url = "%s/%s.TXT" % (BASE_URL, airport)
     req = requests.get(url).text
     for line in req.splitlines():
         if line.startswith(airport):
             obs = Metar.Metar(line)
-            obs = Metar.Metar(
-                "METAR KEWR 111851Z VRB03G19KT 2SM R04R/3000VP6000FT TSRA BR FEW015 "
-                "BKN040CB BKN065 OVC200 22/22 A2987 RMK AO2 PK WND 29028/1817 WSHFT "
-                "1812 TSB05RAB22 SLP114 FRQ LTGICCCCG TS OHD AND NW-N-E MOV NE "
-                "P0013 T02270215")
             text = obs.string()
             print(text)
             tts = gtts.gTTS(text, lang=language)
             tts.save('ema.mp3')
+            ptt.on()
             playsound.playsound('ema.mp3', True)
+            ptt.off()
             break
 
 if __name__ == "__main__":
